269dc8819fcf5d70af3690cfd2fed8eebc3b7d57
eric12s <ericsa@wix.com>
2022-06-06 23:21:12 +0300

    Sketch complete

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -15,31 +15,31 @@ extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 extern uint64 cas(volatile void *address, int prev, int next);
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+struct
+{
   struct spinlock lock;
   struct run *freelist;
-  uint64 count_of_pages[((PHYSTOP-KERNBASE) / PGSIZE)];
+  uint64 count_of_pages[((PHYSTOP - KERNBASE) / PGSIZE)];
 } kmem;
 
-void
-kinit()
+void kinit()
 {
   initlock(&kmem.lock, "kmem");
   memset(kmem.count_of_pages, 0, sizeof(kmem.count_of_pages));
-  freerange(end, (void*)PHYSTOP);
+  freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
-  unit64 index = (((uint64)p-KERNBASE) / PGSIZE);
-  unit64 pa_start_to_roundUp = (uint64)pa_start;
-  char *p = (char*)PGROUNDUP(pa_start_to_roundUp);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+  char *p = (char *)PGROUNDUP((uint64)pa_start);
+  uint64 index = (((uint64)p - KERNBASE) / PGSIZE);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  {
     kmem.count_of_pages[index] = 1;
     kfree(p);
   }
@@ -49,18 +49,20 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  if (dec_ref_count(pa) > 0)
+    return;
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
@@ -78,33 +80,40 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-    
-  if(r)
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+
+  if (r)
     inc_ref_count((uint64)r);
 
-  return (void*)r;
+  return (void *)r;
 }
 
-void inc_ref_count(uint64 pa){
-    uint64 place = pa - KERNBASE;
-    place = place / PGSIZE;
-    while(cas(kmem.count_of_pages + place, kmem.count_of_pages[place], kmem.count_of_pages[place] + 1));
+int inc_ref_count(uint64 pa)
+{
+  uint64 place = pa - KERNBASE;
+  place = place / PGSIZE;
+  while (cas(kmem.count_of_pages + place, kmem.count_of_pages[place], kmem.count_of_pages[place] + 1))
+    ;
+  return get_ref_count(pa);
 }
 
-void dec_ref_count(uint64 pa){
-    uint64 place = pa - KERNBASE;
-    place = place / PGSIZE;
-    while(cas(kmem.count_of_pages + place, kmem.count_of_pages[place], kmem.count_of_pages[place] - 1));
+int dec_ref_count(uint64 pa)
+{
+  uint64 place = pa - KERNBASE;
+  place = place / PGSIZE;
+  while (cas(kmem.count_of_pages + place, kmem.count_of_pages[place], kmem.count_of_pages[place] - 1))
+    ;
+  return get_ref_count(pa);
 }
 
-int get_ref_count(uint64 pa){
-    uint64 place = pa - KERNBASE;
-    place = entry / PGSIZE;
-    return kmem.count_of_pages[place];
+int get_ref_count(uint64 pa)
+{
+  uint64 place = pa - KERNBASE;
+  place = place / PGSIZE;
+  return kmem.count_of_pages[place];
 }
